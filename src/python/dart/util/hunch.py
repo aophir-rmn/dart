681181f5ebc7abe328abe68790bcf0544307588f
Za Irteza <mirteza@rmn.com>
2016-05-09 15:37:36 -0500

    Implement api for metadata sniffer tool (nicknamed hunch) on Datasets #4
    added change from hunch code review. wip #dart-4
    
    response type changed to dataset model. various code improvments. bug fix in reading line to line #4
    
    Added analyze_data(...) to avoid code repetitions #4
    
    remove deprecated comments #4
    
    make return type of analyze row to guesses dict. store guesses in a set instead of dict. better hierarchy logic. #4
    
    guess varchar if null #4

---
+++ src/python/dart/util/hunch.py
@@ -0,0 +1,368 @@
+import boto3
+import bz2
+import csv
+from dart.model.dataset import DataType, Column, Dataset, DatasetData
+from datetime import datetime
+import dateutil.parser
+import magic
+import math
+import re
+from s3 import get_bucket_name, get_key_name
+import zlib
+
+
+def infer_dataset_data(s3_path, max_lines):
+    """
+    :type s3_path: str
+    :param s3_path: The full s3 path to a sample delimited dataset
+        file.
+
+    :type max_lines: int
+    :param max_lines: The maximum number of lines to peek.
+    """
+    guesses, has_header, compression = get_guesses(s3_path, max_lines)
+    columns = columns_with_best_guess(guesses, has_header)
+    location = guess_location(s3_path)
+    return Dataset(data=DatasetData(location=location, columns=columns, compression=compression))
+
+
+def guess_location(s3_path):
+    """
+    :type s3_path: str
+    :param s3_path: The full s3 path to a sample delimited dataset
+        file.
+    """
+    split_path = s3_path.split('/')
+    location = ''
+    for i in xrange(0, len(split_path)-1):
+        location += split_path[i]+'/'
+    return location
+
+
+def get_guesses(s3_path, max_lines=1000):
+    """
+    :type s3_path: str
+    :param s3_path: The full s3 path to a sample delimited dataset
+        file.
+
+    :type max_lines: int
+    :param max_lines: The maximum number of lines to peek.
+    """
+    chunk_size = 1024
+    client = boto3.client('s3')
+    bucket = get_bucket_name(s3_path)
+    key = get_key_name(s3_path)
+    stream = client.get_object(Bucket=bucket, Key=key)['Body']
+    # Read the first chunk of the file stream
+    preview = stream.read(chunk_size)
+    compression = re.search(r'ascii|gzip|bzip', magic.from_buffer(preview).lower()).group(0)
+
+    guess_functions = {
+        'ascii': get_ascii_guesses,
+        'gzip': get_gzip_guesses,
+        'bzip': get_bzip_guesses
+    }
+    return guess_functions[compression](preview, stream, chunk_size, max_lines)
+
+
+def get_ascii_guesses(preview, stream, chunk_size, max_lines):
+    """
+    :type preview: str
+    :param preview: The initial chunk of content read from the s3
+        file stream.
+
+    :type stream: botocore.response.StreamingBody
+    :param stream: StreamingBody object of the s3 dataset file.
+
+    :type chunk_size: int
+    :param chunk_size: Maximum size of the chunk in bytes peeking.
+
+    :type max_lines: int
+    :param max_lines: Maximum number of lines to peek into.
+    """
+    COMPRESSION_TYPE = 'NONE'
+    guesses = dict()
+    dialect = csv.Sniffer().sniff(preview)
+    has_header = csv.Sniffer().has_header(preview)
+    lines_read = 0
+    first_row = True
+    data = ''
+
+    while True:
+        if first_row:
+            chunk = preview
+        else:
+            chunk = stream.read(chunk_size)
+        if not chunk:
+            break
+        data += chunk
+        if '\n' in data:
+            data, max_lines_reached = analyze_data(data, lines_read, max_lines, first_row, guesses,dialect, has_header)
+            first_row = False
+            if max_lines_reached:
+                return guesses, has_header, COMPRESSION_TYPE
+    return guesses, has_header, COMPRESSION_TYPE
+
+
+def get_gzip_guesses(preview, stream, chunk_size, max_lines):
+    """
+    :type preview: str
+    :param preview: The initial chunk of content read from the s3
+        file stream.
+
+    :type stream: botocore.response.StreamingBody
+    :param stream: StreamingBody object of the s3 dataset file.
+
+    :type chunk_size: int
+    :param chunk_size: Maximum size of the chunk in bytes peeking.
+
+    :type max_lines: int
+    :param max_lines: Maximum number of lines to peek into.
+    """
+    COMPRESSION_TYPE = 'GZIP'
+    guesses = dict()
+    dialect = csv.Sniffer().sniff(zlib.decompressobj(zlib.MAX_WBITS|16).decompress(preview))
+    has_header = csv.Sniffer().has_header(zlib.decompressobj(zlib.MAX_WBITS|16).decompress(preview))
+    d = zlib.decompressobj(zlib.MAX_WBITS|16)
+    lines_read = 0
+    first_row = True
+    data = ''
+
+    while True:
+        if first_row:
+            chunk = preview
+        else:
+            chunk = stream.read(chunk_size)
+        if not chunk:
+            break
+        data += d.decompress(chunk)
+        if '\n' in data:
+            data, max_lines_reached = analyze_data(data, lines_read, max_lines, first_row, guesses,dialect, has_header)
+            first_row = False
+            if max_lines_reached:
+                return guesses, has_header, COMPRESSION_TYPE
+    return guesses, has_header, COMPRESSION_TYPE
+
+
+def get_bzip_guesses(preview, stream, chunk_size, max_lines):
+    """
+    :type preview: str
+    :param preview: The initial chunk of content read from the s3
+        file stream.
+
+    :type stream: botocore.response.StreamingBody
+    :param stream: StreamingBody object of the s3 dataset file.
+
+    :type chunk_size: int
+    :param chunk_size: Maximum size of the chunk in bytes peeking.
+
+    :type max_lines: int
+    :param max_lines: Maximum number of lines to peek into.
+    """
+    COMPRESSION_TYPE = 'BZ2'
+    guesses = dict()
+    dialect = csv.Sniffer().sniff(bz2.BZ2Decompressor().decompress(preview))
+    has_header = csv.Sniffer().has_header(bz2.BZ2Decompressor().decompress(preview))
+    lines_read = 0
+    first_row = True
+    data = ''
+
+    while True:
+        if first_row:
+            chunk = preview
+        else:
+            chunk = stream.read(chunk_size)
+        if not chunk:
+            break
+        data += bz2.BZ2Decompressor().decompress(chunk)
+        if '\n' in data:
+            data, max_lines_reached = analyze_data(data, lines_read, max_lines, first_row, guesses,dialect, has_header)
+            first_row = False
+            if max_lines_reached:
+                return guesses, has_header, COMPRESSION_TYPE
+    return guesses, has_header, COMPRESSION_TYPE
+
+
+def analyze_data(data, lines_read, max_lines, first_row, guesses, dialect, has_header):
+    """
+    :type data: str
+    :param data: The data to analyze.
+
+    :type lines_read: int
+    :param lines_read: Total number of lines read so far.
+
+    :type max_lines: int
+    :param max_lines: The maximum number of lines to peek.
+
+    :type first_row: bool
+    :param first_row: True if first row.
+
+    :type guesses: dict
+    :param guesses: The dictionary object containing all guesses.
+
+    :type dialect: csv.Dialect
+    :param dialect: Dialect object derived from the preview of
+        the data file.
+
+    :type has_header: bool
+    :param has_header: True if file has headers.
+    """
+    rows = data.split('\n')
+    if first_row:
+        guesses = analyze_header(rows[0], guesses, dialect, has_header)
+    for row in rows[1:-1]:
+        if lines_read < max_lines:
+            guesses = analyze_row(row, guesses, dialect)
+            lines_read += 1
+        else:
+            return data, True
+    data = ''
+    return data, False
+
+
+def analyze_header(row, guesses, dialect, has_header):
+    """
+    :type row: str
+    :param s3_path: The row to analyze.
+
+    :type guesses: dict
+    :param guesses: The dictionary object containing all guesses.
+
+    :type dialect: csv.Dialect
+    :param dialect: Dialect object derived from the preview of
+        the data file.
+
+    :type has_header: bool
+    :param has_header: True if the file has headers.
+    """
+    if has_header:
+        headers = row.split(dialect.delimiter)
+        for idx, header in enumerate(headers):
+            header = header.strip('"')
+            guesses[idx] = {'name': header, 'guesses': set()}
+    else:
+        headers = row.split(dialect.delimiter)
+        for idx, header in enumerate(headers):
+            guesses[idx] = {'guesses': set()}
+    return guesses
+
+
+def analyze_row(row, guesses, dialect):
+    """
+    :type row: str
+    :param s3_path: The row to analyze.
+
+    :type guesses: dict
+    :param guesses: The dictionary object containing all guesses.
+
+    :type dialect: csv.Dialect
+    :param dialect: Dialect object derived from the preview of
+        the data file.
+    """
+    cur_row = row.split(dialect.delimiter)
+    for idx, value in enumerate(cur_row):
+        value = value.strip('"')
+        guess = guess_data_type(value)
+        guesses[idx]['guess_set'].add(guess)
+        if guess == DataType.VARCHAR:
+            current_max = guesses[idx].get('varcharlength', 0)
+            guesses[idx]['varcharlength'] = max(current_max, len(value))
+        if guess == 'null':
+            guesses[idx]['varcharlength'] = 1
+    return guesses
+
+
+def guess_data_type(value):
+    """
+    :param value: The value to infer.
+    """
+    if value.lower() == 'null':
+        return 'null'
+    try:
+        retval = float(value)
+        if retval.is_integer():
+            if 0 <= retval <= 1:
+                return DataType.BOOLEAN
+            elif -32768 <= retval <= 32767:
+                return DataType.SMALLINT
+            elif -2147483648 <= retval <= 2147483647:
+                return DataType.INT
+            elif -9223372036854775808 <= retval <= 9223372036854775807:
+                return DataType.BIGINT
+        else:
+            if -3.4 * math.pow(10,38) <= value <= -1.18 * math.pow(10,-38) or 1.18 * math.pow(10,-38) <= value <= 3.4 * math.pow(10,38):
+                return DataType.FLOAT
+            else:
+                return DataType.NUMERIC
+    except:
+        try:
+            retval = str(value)
+            if retval.lower() == 'true' or retval.lower() == 'false':
+                return DataType.BOOLEAN
+            else:
+                try:
+                    time = dateutil.parser(value)
+                    if datetime(1970,1,1,0,0,1) <= time <= datetime(2038,1,19,3,14,7):
+                        return DataType.TIMESTAMP
+                    else:
+                        return DataType.DATETIME
+                except:
+                    try:
+                        datetime.strptime(value, '%Y-%m-%d')
+                        return DataType.DATE
+                    except:
+                        return DataType.VARCHAR
+        except:
+            return 'null'
+
+
+def best_guess(guess_set):
+    """
+    :type guess_set: set
+    :param guess_set: The set contatining all possible guesses
+        for a certain column.
+    """
+    hierarchy = {
+        DataType.VARCHAR: 10,
+        DataType.TIMESTAMP: 9,
+        DataType.DATETIME: 8,
+        DataType.DATE: 7,
+        DataType.NUMERIC: 6,
+        DataType.FLOAT: 5,
+        DataType.BIGINT: 4,
+        DataType.INT: 3,
+        DataType.SMALLINT: 2,
+        DataType.BOOLEAN: 1,
+        'null': 0
+    }
+    max_val = -1
+    for guess in guess_set:
+        if hierarchy[guess] > max_val:
+            max_val = hierarchy[guess]
+            best = guess
+    return best
+
+
+def columns_with_best_guess(guesses, has_header):
+    """
+    :type guesses: dict
+    :param guesses: Dictionary that represents all guesses for
+        each column in the dataset.
+
+    :type has_header: bool
+    :param has_header: True if file has header.
+    """
+    columns = []
+    for idx in guesses:
+        guess_set = guesses[idx]['guess_set']
+        data_type = best_guess(guess_set)
+        if data_type == DataType.VARCHAR or data_type == 'null':
+            max_len = guesses[idx]['varcharlength']
+            length = 2 ** (int(math.log(max_len, 2)) + 1)
+            data_type = DataType.VARCHAR
+
+        columns.append(Column
+                       (name=guesses[idx]['name'] if has_header else 'col_{}'.format(idx),
+                        data_type=data_type,
+                        length=length if length else 0))
+    return columns
--- src/python/dart/web/api/dataset.py
+++ src/python/dart/web/api/dataset.py
@@ -4,9 +4,11 @@ from flask import Blueprint, request, current_app
 from flask.ext.jsontools import jsonapi
 
 from dart.model.dataset import Dataset
+from dart.schema.dataset import dataset_schema
 from dart.service.dataset import DatasetService
 from dart.service.filter import FilterService
 from dart.web.api.entity_lookup import fetch_model
+from dart.util.hunch import infer_dataset_data
 
 api_dataset_bp = Blueprint('api_dataset', __name__)
 
@@ -22,6 +24,17 @@ def post_dataset():
 @jsonapi
 def get_dataset(dataset):
     return {'results': dataset.to_dict()}
+
+
+@api_dataset_bp.route('/dataset/guess', methods=['GET'])
+@jsonapi
+def get_dataset_guess():
+    s3_path = request.args.get('s3path')
+    max_lines = request.args.get('maxlines')
+    best_guess_schema = dataset_schema()
+    best_guess_dataset = infer_dataset_data(s3_path, max_lines)
+    best_guess_schema['properties']['data']['properties']['columns'] = best_guess_dataset.to_dict()
+    return {'results': best_guess_schema}
 
 
 @api_dataset_bp.route('/dataset', methods=['GET'])
