1323901fe450071b6239a61dd39ff262048557fe
Adi Ophir <aophir@rmn.com>
2016-06-10 17:58:25 -0500

    Updated local setup to a vagrqnt file that creates a VMa and then a script that uses docker-compose to launch elasticmq/postgresql/web
    Got partial unit tests + local deploy working
    
    Switched to use jinga files.
    
    Using volumes so we can develop against the code in ./dart (in the directory where we run setup.sh)
    
    Adding a deployment unit tests that is run during the setup script
    
    Adding more README comments
    
    Adding initial travis.yml file
    
    Making sure we can generate the config files from the root of the proejct by calling python tools/vagrant/generate_deployment_files.py
    
    Using docker-compose in travis-ci
    
    Added setup script fix
    
    Adding one-line checks to make sure the containers are up
    
    Adding fixed unit tests from RMN fork
    
    Removing properiatry config info

--- tools/vagrant/setup.sh
+++ tools/vagrant/setup.sh
@@ -1,51 +1,26 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-set +x
-
-# The epel-release has repos for other downloads.
-# we get the tools needed to install dart locally.
-sudo yum install epel-release -y
-sudo yum install vim-enhanced mlocate git docker docker-registry python-pip python-wheel python-pip python-wheel postgresql-devel python-devel npm  -y
-
-# Get the dart repo. We need the ssh keys (stored in the ./ssh_files) in order to clone.
-HOME_DIR=/home/vagrant
-PROJ_DIR=${HOME_DIR}/projects
-echo $PROJ_DIR
-   # placing ssh keys in root and /home/vagrant directories so we can git clone as both users
-   sudo mkdir -p /root/.ssh &&
-   sudo cp /vagrant_data/ssh_files/* /root/.ssh &&
-   mkdir -p ${HOME_DIR}/.ssh &&
-   cp /vagrant_data/ssh_files/* /${HOME_DIR}/.ssh &&
-   # download the dart and dart-config repos to ~/projects
-   mkdir -p /home/vagrant/projects && 
-   cd /home/vagrant/projects && 
-   git clone https://github.com/RetailMeNotSandbox/dart.git &&
-   git clone ssh://git@stash.rmn.com:7999/data/dart-config.git  
-
-# pip install requirements for the python project   
-cd ${PROJ_DIR}/dart/src/python/ && sudo pip install --upgrade pip &&  sudo pip install -r requirements.txt
-
-# install UI (e.g. angular) packages
-# setup bower (used for dart UI)
-sudo npm install bower -g && cd /home/vagrant/projects/dart/src/python/dart/web/ui && bower install
-
-# Needed in order to use the docker provisioner
-# we also move Dockerfiles to their own directories so they can be named Dockerfile and we can use the vagrant provisioner
-sudo groupadd -f docker
-mkdir -p /tmp/elasticmq && cp /home/vagrant/projects/dart/tools/docker/Dockerfile-elasticmq /tmp/elasticmq/Dockerfile
-sudo updatedb # indexes all used files
-
-# setup env variables:
-# To make sure they will be set next time we login to a session
-echo "export AWS_SECRET_ACCESS_KEY=value" > /home/vagrant/.bashrc &&
-echo "export AWS_SECRET_ACCESS_KEY=value" >> /home/vagrant/.bashrc &&
-echo "export DART_ROLE=web" >> /home/vagrant/.bashrc &&
-echo "export DART_CONFIG=/home/vagrant/projects/dart-config/dart-local.yaml" >> /home/vagrant/.bashrc
-echo "export PYTHONPATH=/home/vagrant/projects/dart/src/python" >> /home/vagrant/.bashrc
-
-# preparing to run the webserver using the same env variables as above
-export AWS_SECRET_ACCESS_KEY=1 && export AWS_SECRET_ACCESS_KEY=2 && export DART_ROLE=web && export DART_CONFIG=/home/vagrant/projects/dart-config/dart-local.yaml && export PYTHONPATH=/home/vagrant/projects/dart/src/python
+# Start directory is tools/vagrant folder in dart repo (git clone https://github.com/RetailMeNotSandbox/dart.git)
+set -x # echo command
+set -e # exit on first error
 
 
-### launch the web server
-cd ${PROJ_DIR}/dart/src/python/dart/web && python  ./server.py
+## run unit-tests
+python tests/test_generate_deployment_files.py
+
+# We will read the code from this repo - you can change code and it will be seen in:
+# Vagrant: /vagrant_Data
+# Docker-containers: /Code
+rm -rf ./dart
+git clone https://github.com/RetailMeNotSandbox/dart.git 
+
+# Generates Dokcer-compose, dockerfiles and config files from jinja templates.
+python generate_deployment_files.py
+
+# We build a VM (Using vagrant => vagrant up) that will have docker tools installed and other useful pre-reqs.
+vagrant plugin install vagrant-triggers
+vagrant plugin install vagrant-vbguest
+vagrant up
+
+## Launche docker-compose that launches dart-web, dart-postgresql and dart-elastic (elasticmq) as well as the trigger/subscription and engine workers.
+vagrant ssh -c "~/additional_steps.sh"
