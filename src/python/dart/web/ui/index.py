00c15a87fa70f093f571a71ba42b3538d44841d9
Adi Ophir <aophir@rmn.com>
2016-09-19 13:34:56 -0500

    Removing login check before index.py

--- src/python/dart/web/ui/index.py
+++ src/python/dart/web/ui/index.py
@@ -5,11 +5,12 @@ index_bp = Blueprint('index', __name__)
 
 
 @index_bp.route('/')
-@check_login
 def index():
+    print "### index '/' %s " % session.get("user_id")
     # Forcing user to authenticate if he is not.
     # This is a work around since python-saml & Flask-login should have made this redirect.
     if not session.get("user_id"):
+        print "### render 'info'"
         return render_template("info.html")
 
     return render_template('index.html')
