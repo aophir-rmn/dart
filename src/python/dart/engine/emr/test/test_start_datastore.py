09906be35339e154e57624bc0e92769116b35c9c
Adi Ophir <aophir@rmn.com>
2017-02-20 17:13:22 -0600

    Fixing unittest for Travis CI

--- src/python/dart/engine/emr/test/test_start_datastore.py
+++ src/python/dart/engine/emr/test/test_start_datastore.py
@@ -93,7 +93,7 @@ class TestStartDatastoreAction(unittest.TestCase):
                 'Function': 'a-b',
                 'Accounting': '222-1111111'
             },
-            cluster_availability_zone='regionb',
+            #cluster_availability_zone='regionb',
             dart_host='somehost',
             dart_port=5000
         )
